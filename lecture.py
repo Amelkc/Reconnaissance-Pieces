81d994367e7ba80f3e067cd493d9e92e43fabc1d
Clara AM <rabiaautop@gmail.com>
2026-02-06 17:10:08 +0100

    mini modif

--- lecture.py
+++ lecture.py
@@ -1,15 +1,5 @@
-import pandas as pd
+import cv2
+import numpy as np
+import glob
+import os
 
-df = pd.read_csv('data.csv', header=1)
-
-
-print("DATA :")
-print(df.head())
-
-
-for index, row in df.iterrows():
-    nom_image = row['Nom image']
-    nb_pieces_reel = row['Nombre de pièces']
-    valeur_reelle = row['Valeur monétaire €']
-    
-    print(f"Image : {nom_image} | Vérité : {nb_pieces_reel} pièces, {valeur_reelle} €")
